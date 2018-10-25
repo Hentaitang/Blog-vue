0b0a10489b64d5118130bab0bc088c0f68245b48
Hentaitang <mz374427166@gmail.com>
2018-10-25 13:46:39 +0800

    添加数据接口交互

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,8 +1,31 @@
+import request from '@/helpers/request.js'
+import auth from '@/api/auth.js'
+import blog from '@/api/blog.js'
+window.request = request
+window.auth = auth
+window.blog = blog
+
 export default {
-    name: 'HelloWorld',
-    data () {
-      return {
-        msg: 'Welcome to Your Vue.js App'
+    methods: {
+      click1(){
+        this.$message({
+          message: '恭喜你，这是一条成功消息',
+          type: 'success'
+        })
+      },
+      click2(){
+        this.$message.error('错了哦，这是一条错误消息');
+      },
+      click3(){
+        this.$alert('这是一段内容', '标题名称', {
+          confirmButtonText: '确定',
+          callback: action => {
+            this.$message({
+              type: 'success',
+              message: `点了确定`
+            })
+          }
+        })
       }
     }
   }
--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="index">
     <p>首页</p>
+    <el-button @click="click1">成功</el-button>
+    <el-button @click="click2">失败</el-button>
+    <el-button @click="click3">消息提示</el-button>
   </div>
 </template>
 <script src="./template.js"></script>
