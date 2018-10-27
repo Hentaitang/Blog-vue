c86b7bae63876f15b63e6d83f122fddf48647daa
Hentaitang <mz374427166@gmail.com>
2018-10-28 00:26:40 +0800

    首页展示样式，使用grid布局

--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,9 +1,23 @@
 <template>
-  <div class="index">
-    <p>首页</p>
-    <el-button @click="click1">成功</el-button>
-    <el-button @click="click2">失败</el-button>
-    <el-button @click="click3">消息提示</el-button>
+  <div id="index">
+    <section>
+      <div class="item">
+        <figure class="avatar">
+          <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="">
+          <figcaption>白兔糖</figcaption>
+        </figure>
+        <h3>前端异步大揭秘 <span>3天前</span></h3>
+        <p>本文以一个简单的文件读写为例，讲解了异步的不同写法，包括 普通的 callback、ES2016中的Promise和Generator、 Node 用于解决回调的co 模块、ES2017中的async/await。适合初步接触 Node.js以及少量 ES6语法的同学阅读...</p>
+      </div>
+      <div class="item">
+        <figure class="avatar">
+          <img src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="">
+          <figcaption>白兔糖</figcaption>
+        </figure>
+        <h3>前端异步大揭秘 <span>3天前</span></h3>
+        <p>本文以一个简单的文件读写为例，讲解了异步的不同写法，包括 普通的 callback、ES2016中的Promise和Generator、 Node 用于解决回调的co 模块、ES2017中的async/await。适合初步接触 Node.js以及少量 ES6语法的同学阅读...</p>
+      </div>
+    </section>
   </div>
 </template>
 <script src="./template.js"></script>
