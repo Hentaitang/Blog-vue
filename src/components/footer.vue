d428842c615e1f99a1e66b4ef98690d7eafe7544
Hentaitang <mz374427166@gmail.com>
2018-10-27 22:11:02 +0800

    添加html及页面样式

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <p>© baitutang.com 2018 wechat: lzx374427166</p>
+  </footer>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+  #footer{
+    background-color: #d7d7d7;
+    text-align: center;
+    color: #666;
+    padding: 10px;
+    font-size: 13px;
+  }
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <h1>Let's share</h1>
+    <p>精品博客汇聚</p>
+    <div class="bts">
+      <el-button>立即登录</el-button>
+      <el-button>注册账号</el-button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less">
+  @import '../assets/base.less';
+  #header{
+    padding: 0 12% 30px 12%;
+    background: @bgColor;
+    display: grid;
+    justify-items: center;
+
+    h1{
+      color: #fff;
+      font-size: 40px;
+      margin: 60px 0 0;
+      text-transform: uppercase;
+    }
+
+    p{
+      margin: 15px 0 0;
+      color: #fff;
+    }
+
+    button{
+      margin: 40px 5px 0;
+    }
+  }
+</style>
