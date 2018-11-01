28bb73615724382e938cc6054ecdda36e4425cf7
Hentaitang <mz374427166@gmail.com>
2018-11-01 23:07:46 +0800

    添加vuex

--- src/pages/User/template.vue
+++ src/pages/User/template.vue
@@ -22,7 +22,7 @@
           <p class="year">2018</p>
         </div>
         <h3>前端异步解密</h3>
-        <p>本文以一个简单的文件读写为例，讲解了异步的不同写法，包括  普通的 callback、ES2016中的Promise和Generator、 Node 用于解决回调的co 模块、ES2017中的async/await。适合初步接触 Node.js以及少量 ES6语法的同学阅读...</p>
+        <p>本文以一个简单的文件读写为例，讲解了异步的不同写法，包括 普通的 callback、ES2016中的Promise和Generator、 Node 用于解决回调的co 模块、ES2017中的async/await。适合初步接触 Node.js以及少量 ES6语法的同学阅读...</p>
       </div>
     </section>
     <el-pagination
