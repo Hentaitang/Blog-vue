2d5202be184d8c28a0f7b78d52c05199ad8cbfa8
Hentaitang <mz374427166@gmail.com>
2018-11-02 18:02:08 +0800

    懒加载已经项目打包

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/pages/Index/template.vue'
-import Login from '@/pages/Login/template.vue'
-import Register from '@/pages/Register/template.vue'
-import Detail from '@/pages/Detail/template.vue'
-import Edit from '@/pages/Edit/template.vue'
-import User from '@/pages/User/template.vue'
-import My from '@/pages/My/template.vue'
-import Create from '@/pages/Create/template.vue'
 import Store from '../store'
 
 Vue.use(Router)
@@ -16,37 +8,37 @@ const router =  new Router({
   routes: [
     {
       path: '/',
-      component: Index
+      component: () => import('@/pages/Index/template.vue')
     },
     {
       path: '/login',
-      component: Login
+      component: () => import('@/pages/Login/template.vue')
     },
     {
       path: '/register',
-      component: Register
+      component: () => import('@/pages/Register/template.vue')
     },
     {
       path: '/my',
-      component: My,
+      component: () => import('@/pages/My/template.vue'),
       meta: {requiresAuth: true}
     },
     {
       path: '/user/:userId',
-      component: User
+      component: () => import('@/pages/User/template.vue')
     },
     {
       path: '/edit/:blogId',
-      component: Edit,
+      component: () => import('@/pages/Edit/template.vue'),
       meta: {requiresAuth: true}
     },
     {
       path: '/detail/:blogId',
-      component: Detail
+      component: () => import('@/pages/Detail/template.vue')
     },
     {
       path: '/create',
-      component: Create,
+      component: () => import('@/pages/Create/template.vue'),
       meta: {requiresAuth: true}
     }
   ]
